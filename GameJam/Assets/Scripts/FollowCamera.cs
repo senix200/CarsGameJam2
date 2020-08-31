4bb66865e83eca1140f2bca8225119ad1123b0c0
Sergio González <s3rgi0trunks@hotmail.com>
2020-08-31 10:59:59 +0200

    Camera and Spawns

---
+++ GameJam/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform car;
+    public float distance;
+    public float height;
+    public float rotationDamping;
+    public float heightDamping;
+    public float zoomRatio;
+    public float defaultFOV;
+    public float rotation_vector;
+
+   void FixedUpdate(){
+        
+        }
+    void LateUpdate(){
+        
+        }
+}
---
+++ GameJam/Assets/Scripts/TileManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+
+    public GameObject[] titlePrefabs;
+
+    private Transform playerTransform;
+    private float spawnZ = 0.0f;
+    private float tileLength = -197.0f;
+    private int amnTilesOnScreen = 120;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        for (int i = 0; i < amnTilesOnScreen; i++)
+        {
+            SpawnTile();
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (playerTransform.position.z > (spawnZ - amnTilesOnScreen * tileLength))
+        {
+            SpawnTile();
+        }
+    }
+
+    private void SpawnTile(int prefabIndex = -1){
+        GameObject go;
+        go = Instantiate (titlePrefabs [0]) as GameObject;
+        go.transform.SetParent (transform);
+        go.transform.position = Vector3.forward * spawnZ;
+        spawnZ+=tileLength;
+    }
+}
